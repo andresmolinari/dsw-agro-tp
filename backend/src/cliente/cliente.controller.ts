e8a9615e82edd73b818918bf4d0f9c753a95798a
Franco Moreal <francomoreal@gmail.com>
2024-05-23 16:50:45 -0300

    test1

--- backend/src/cliente/cliente.controller.ts
+++ backend/src/cliente/cliente.controller.ts
@@ -1,12 +1,10 @@
-import { Request, Response, NextFunction } from "express"
-import { ClienteRepository } from "./cliente.repository.js"
-import { Cliente } from "./cliente.entity.js"
+import { Request, Response, NextFunction } from 'express';
+import { ClienteRepository } from './cliente.repository.js';
 
-const repository = new ClienteRepository //seria el acceso a la base de datos
+const repository = new ClienteRepository(); //seria el acceso a la base de datos
 
 // sanitizacion middleware
 function sanitizeClienteInput(req: Request, res: Response, next: NextFunction) {
-
   req.body.sanitizedCliente = {
     nroCliente: req.body.nroCliente,
     nombre: req.body.nombre,
@@ -15,26 +13,23 @@ function sanitizeClienteInput(req: Request, res: Response, next: NextFunction) {
     direccion: req.body.direccion,
     localidad: req.body.localidad,
     provincia: req.body.provincia,
-  }
+  };
 
   //nos quedamos con los elementos que son not null para el patch
   Object.keys(req.body.sanitizedCliente).forEach((key) => {
     if (req.body.sanitizedCliente[key] === undefined) {
-      delete req.body.sanitizedCliente[key]
+      delete req.body.sanitizedCliente[key];
     }
-  })
-  next()
+  });
+  next();
 }
 
 // obtener todos los clientes
 function findAll(req: Request, res: Response) {
-  res.json({ data: repository.findAll() })
+  res.json({ data: repository.findAll() });
 }
 
 export const controller = {
   sanitizeClienteInput,
   findAll,
-
-
-}
-
+};
