43989cd59ee6e2a24eb40ae5fca5d365a59c439c
andresmolinari <83131746+andresmolinari@users.noreply.github.com>
2024-05-23 16:27:31 -0300

    Delete backend/dist directory

--- backend/dist/cliente/cliente.routes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { controller } from "./cliente.controller.js";
-export const clienteRouter = Router();
-clienteRouter.get('/', controller.findAll);
-//# sourceMappingURL=cliente.routes.js.map
