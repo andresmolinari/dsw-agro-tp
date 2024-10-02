31e6bacf3913f5630f19f9ce164a887b904e78d6
Franco Moreal <francomoreal@gmail.com>
2024-10-01 23:31:16 -0300

    Login y routes

--- frontend/src/router/AppRouter.tsx
+++ frontend/src/router/AppRouter.tsx
@@ -1,15 +1,15 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { AppLayout } from '../common/AppLayout';
-import HomePage from '../pages/Home'; // Corrige el nombre del componente
-import LoginPage from '../pages/Login'; // Corrige el nombre del componente
+import HomePage from '../pages/Home';
+import LoginPage from '../pages/Login';
 
 export const AppRouter = () => {
   return (
     <Routes>
       <Route path='/' element={<AppLayout />}>
-        <Route path='login' element={<LoginPage />} />
         <Route path='home' element={<HomePage />} />
       </Route>
+      <Route path='login' element={<LoginPage />} />
       <Route path='*' element={<Navigate to='/' />} />
     </Routes>
   );
