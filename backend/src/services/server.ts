96ac885b995472a79382d32d86254b072bb32011
Andrés Molinari <andymolinari18@gmail.com>
2025-11-17 16:50:16 -0300

    await sequelize.sync()

--- backend/src/services/server.ts
+++ backend/src/services/server.ts
@@ -67,8 +67,8 @@ class Server {
     try {
       defineAssociations();
       //await sequelize.authenticate();
-      //await sequelize.sync();
-      await sequelize.sync({ force: true }); //borra las creadas y las vuelve a crear
+      await sequelize.sync();
+      //await sequelize.sync({ force: true }); //borra las creadas y las vuelve a crear
       //await sequelize.sync({ alter: true }); // chequea si hay cambios y los hace
       console.log('Tablas sincronizadas');
 
